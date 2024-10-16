a3b25e4ecab41a09b8d2a2bdca3b3e4a9e99703e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-10-16 13:40:42 -0700

    baymax

---
+++ src/routes/day4/+page.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+	<style>
+		body {
+			background-color: #ececec;
+		}
+	</style>
+</svelte:head>
+
+<main class="py-4 max-w-screen-md mx-auto relative h-screen">
+	<img src="/baymax/leg.svg" alt="Baymax Leg" class="absolute left-[28%] top-[77%] -scale-x-100" />
+	<img src="/baymax/leg.svg" alt="Baymax Leg" class="absolute right-[28%] top-[77%]" />
+
+	<img src="/baymax/body.svg" alt="Baymax Body" class="absolute top-[28%] left-0 right-0 mx-auto" />
+
+	<img src="/baymax/arm.svg" alt="Baymax Arm" class="absolute top-[35%] left-[12%] -scale-x-100" />
+	<img src="/baymax/arm.svg" alt="Baymax Arm" class="absolute top-[35%] right-[12%]" />
+
+	<img src="/baymax/head.svg" alt="Baymax Head" class="absolute left-0 right-0 mx-auto" />
+
+	<div class="absolute top-[18%] left-[35%] w-6 h-6 bg-black rounded-full"></div>
+	<div class="absolute top-[18%] left-[61%] w-6 h-6 bg-black rounded-full"></div>
+	<img
+		src="/baymax/mouth.svg"
+		alt="Baymax Mouth"
+		class="absolute left-0 right-0 mx-auto top-[20%]"
+	/>
+</main>
