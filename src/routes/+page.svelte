b6119141c7b9d69d6ff34705726937e4d8daad8a
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-10-16 11:55:08 -0700

    initial commit :D

---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<main class="flex flex-col sm:flex-row gap-4">
+	<section class="flex flex-col gap-2 p-8 max-w-[768px]">
+		<h1 class="text-4xl font-semibold">100 Days Of Design Challenge</h1>
+		<p>
+			A compilation of some of ACM CSUF's <span>Design Team</span> favorite designs from the
+			<b>100 Days of Design</b> Challenge.
+		</p>
+		<div class="pl-4 max-w-max">
+			<a class="group flex gap-1" href="/day2">
+				<span class="group-hover:underline underline-offset-2"> Day 2 Submission </span>
+				<img
+					src="/diagonalarrow.svg"
+					alt="Link Icon"
+					class="group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform"
+				/>
+			</a>
+			<a class="group flex gap-1" href="/day3">
+				<span class="group-hover:underline underline-offset-2"> Day 3 Submission </span>
+				<img
+					src="/diagonalarrow.svg"
+					alt="Link Icon"
+					class="group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform"
+				/>
+			</a>
+			<a class="group flex gap-1" href="/day4">
+				<span class="group-hover:underline underline-offset-2"> Day 4 Submission </span>
+				<img
+					src="/diagonalarrow.svg"
+					alt="Link Icon"
+					class="group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform"
+				/>
+			</a>
+			<a class="group flex gap-1" href="/day9">
+				<span class="group-hover:underline underline-offset-2"> Day 9 Submission </span>
+				<img
+					src="/diagonalarrow.svg"
+					alt="Link Icon"
+					class="group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform"
+				/>
+			</a>
+		</div>
+	</section>
+	<img src="/acmdesign.svg" alt="ACM Design Icon" class="max-w-[24rem]" />
+</main>
+
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+	main {
+		font-family: "Poppins", sans-serif;
+	}
+</style>
