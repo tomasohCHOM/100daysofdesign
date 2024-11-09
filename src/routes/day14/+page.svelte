710b72ca74607a9c365f4cae8b664b13743b6ba8
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-11-09 12:34:01 -0800

    start day14

--- src/routes/day14/+page.svelte
+++ src/routes/day14/+page.svelte
@@ -9,34 +9,49 @@
 	</style>
 </svelte:head>
 
-<div class="flex h-screen w-full items-center justify-center">
-	<main class="relative h-full w-[min(1024px,90vw)]">
-		<div class="absolute left-0 right-0 top-1/3">
-			<h1 class="text-center text-5xl">DEBUT ALBUM RELEASE</h1>
-			<div class="flex items-center justify-center">
-				<div class="rounded-xl p-4">
-					<span>63</span>
-					<span>days</span>
+<div class="flex h-[668.8px] w-full items-center justify-center">
+	<main class="relative h-full w-[min(1024px,90vw)] border-2 border-white">
+		<div class="flex h-full flex-col">
+			<h1 class="mt-48 text-center text-5xl">DEBUT ALBUM RELEASE</h1>
+			<div class="mt-4 flex items-center justify-center gap-6">
+				<div class="flex flex-col items-center rounded-xl bg-[#d7ffb0] bg-opacity-25 p-4">
+					<span class="text-6xl">63</span>
+					<span class="text-2xl">days</span>
 				</div>
-				<div>
-					<span>9</span>
-					<span>hours</span>
+				<div class="flex flex-col items-center rounded-xl bg-[#d7ffb0] bg-opacity-25 p-4">
+					<span class="text-6xl">9</span>
+					<span class="text-2xl">hours</span>
 				</div>
-				<div>
-					<span>10</span>
-					<span>minutes</span>
+				<div class="flex flex-col items-center rounded-xl bg-[#d7ffb0] bg-opacity-25 p-4">
+					<span class="text-6xl">10</span>
+					<span class="text-2xl">minutes</span>
 				</div>
 			</div>
+
+			<a
+				href="https://open.spotify.com/prerelease/5nlyRaHZKCezRCf1Buhtkm?si=84e1952d1f7f4161&nd=1&dlsi=32786b390d9942e5"
+				class="mx-auto mt-8 block max-w-max text-center text-2xl underline"
+				target="_blank"
+			>
+				Pre-Save Escaper here
+			</a>
+
+			<footer class="mb-4 mt-auto flex items-center justify-between px-4">
+				<div class="flex items-center gap-1 text-[0.75rem] sm:text-[1rem]">
+					<img src="/debut/spotify.svg" alt="Spotify Icon" class="w-6" /> Sarah Kinsley
+				</div>
+				<p class="text-[0.625rem] sm:text-[1rem]">STREAM 'Last Time We Never Meet again' NOW</p>
+			</footer>
 		</div>
 	</main>
 </div>
 
 <style>
 	main {
-		background: url("/debut/album-image.png") rgba(205, 190, 175, 0.4) 100% / cover no-repeat;
+		background: url("/debut/album-image.png") rgba(176, 160, 144, 0.5) 100% / cover no-repeat;
 		background-size: contain;
 		background-repeat: no-repeat;
 		background-blend-mode: overlay;
-		padding-top: 65.31%;
+		/* padding-top: 65.31%; */
 	}
 </style>
