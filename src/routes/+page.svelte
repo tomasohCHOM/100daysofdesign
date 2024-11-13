98b03b68fb6c888ea18f7a426da980b889660e86
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-11-12 20:44:16 -0800

    almost done with landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,24 @@
 </script>
 
 <div class="mt-8 flex flex-col items-center">
-	<div class="ball relative">
+	<div class="ball relative h-[350px] md:h-[600px]">
 		<img
 			src="/acm.svg"
 			alt="ACM Icon"
-			class="absolute left-1/2 top-[40%] max-w-[16rem] -translate-x-1/2 -translate-y-1/2"
+			class="absolute left-1/2 top-[40%] max-w-[8rem] -translate-x-1/2 -translate-y-1/2 md:max-w-[16rem]"
+		/>
+		<img src="flower.svg" alt="Flower" class="absolute -left-28 bottom-6 z-20 w-40" />
+		<img src="flower-faded.svg" alt="Flower" class="absolute -right-28 bottom-6 z-20 w-40" />
+
+		<img src="/capy1.svg" alt="Capybara 1" class="absolute bottom-6 left-28 z-20 w-40" />
+		<img src="/capy2.svg" alt="Capybara 1" class="absolute bottom-6 right-28 z-20 w-40" />
+		<div
+			class="absolute -bottom-16 left-1/2 h-32 w-[20rem] -translate-x-1/2 rounded-full bg-[#fc7373] md:w-[30rem]"
 		/>
 	</div>
-	<main class="flex h-[80vh] w-full flex-col gap-4 bg-[#fc7373] text-white sm:flex-row">
+	<main class="flex h-[80vh] w-full flex-col gap-4 bg-[#fc7373] py-24 text-white sm:flex-row">
 		<section class="flex max-w-[768px] flex-col gap-2 p-8">
-			<h1 class="text-4xl font-semibold">100 Days Of Design Challenge</h1>
+			<h1 class="text-4xl font-bold">100 Days Of Design Challenge</h1>
 			<p>
 				A compilation of some of ACM CSUF's <span>Design Team</span> favorite designs from the
 				<b>100 Days of Design</b> Challenge.
@@ -53,7 +61,7 @@
 	}
 
 	.ball {
-		height: 100vh;
+		/* height: 600px; */
 		aspect-ratio: 1;
 		border-radius: 50%;
 		background: radial-gradient(50% 50% at 50% 50%, #ffd7d7 0%, #ffacac 100%);
